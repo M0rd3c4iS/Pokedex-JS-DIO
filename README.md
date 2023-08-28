ffbdc90098985d2e6e057c5c292495def2acd4b9
pauloericles <pauloericles215@hotmail.com>
2023-08-28 03:08:38 -0300

    Subindo arquivos e estrutura

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Pokedex-JS-DIO
 Pokedex feita para o curso de JS da DIO-Digital Inovation. 
+
+Commit-1 arquivos e estrutura do site upadas.
---
+++ pokedex/assets/js/poke-api.js
@@ -0,0 +1,39 @@
+
+const pokeApi = {}
+
+function convertPokeApiDetailToPokemon(pokemonDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokemonDetail.id
+    pokemon.name = pokemonDetail.name
+
+    const types = pokemonDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type1] = types
+
+    pokemon.types = types
+    pokemon.type = type1
+
+    pokemon.photo = pokemonDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+
+pokeApi.getPokemons = (offset = 0, limit = 20) => {
+    const ulr = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+
+    return fetch(ulr)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonDetails) => pokemonDetails)
+}
+
+
+
